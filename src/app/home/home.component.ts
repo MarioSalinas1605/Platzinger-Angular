4b54d51792c3111d7cb49f2251d581da86e8f934
MarioSalinas <mario_1605@live.com.mx>
2018-11-17 08:34:22 -0600

    Interface to User

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../interfaces/user';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    let myUser: User = {
+      nick: 'Eduardo',
+      subnick: 'HolaLalo',
+      age: 28,
+      email: 'lalo@estimados.com',
+      friend: true,
+      uid: 1
+    }
+    console.log(myUser)
+   }
 
   ngOnInit() {
   }
