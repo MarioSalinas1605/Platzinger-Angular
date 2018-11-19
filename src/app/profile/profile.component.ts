2344e428fda72344914037126dc77070e0952537
MarioSalinas <mario_1605@live.com.mx>
2018-11-19 11:16:35 -0600

    Saving profile photo on Firebase

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -3,6 +3,7 @@ import { User } from '../interfaces/user';
 import { UserService } from '../services/user.service';
 import { AuthenticationService } from '../services/authentication.service';
 import { ImageCroppedEvent } from 'ngx-image-cropper/src/image-cropper.component';
+import { AngularFireStorage } from '@angular/fire/storage';
 
 @Component({
   selector: 'app-profile',
@@ -13,7 +14,8 @@ export class ProfileComponent implements OnInit {
   user: User
   imageChangedEvent: any = '';
   croppedImage: any = '';
-  constructor(private userService: UserService, private authenticationService: AuthenticationService) {
+  picture: any;
+  constructor(private userService: UserService, private authenticationService: AuthenticationService, private firebaseStorage: AngularFireStorage) {
     this.authenticationService.getStatus().subscribe(
       (status)=>{
         this.userService.getUserById(status.uid).valueChanges().subscribe(
@@ -36,13 +38,41 @@ export class ProfileComponent implements OnInit {
   }
 
   saveSettings(){
-    this.userService.editUser(this.user)
-    .then(()=>{
-      alert('Cambios guardados')
-    })
-    .catch((error)=>{
-      console.log(error)
-    })
+    if (this.croppedImage) {
+      const currentPicturedId = Date.now()
+      const pictures = this.firebaseStorage.ref('pictures/'+currentPicturedId+'.jpg')
+      .putString(this.croppedImage, 'data_url')
+      pictures.then(
+        (result)=>{
+          this.picture = this.firebaseStorage.ref('pictures/'+currentPicturedId+'.jpg').getDownloadURL()
+          this.picture.subscribe(
+            (p)=>{
+              this.userService.setAvatar(p, this.user.uid).then(()=>{
+                alert('Avatar subido correctamente')
+              })
+              .catch((error)=>{
+                alert('Hubo un error al subir la imagen')
+                console.log(error)
+              })
+          })
+        }
+      )
+      .catch(
+        (error)=>{
+          console.log(error)
+        }
+      )
+    }
+    else{
+      this.userService.editUser(this.user)
+      .then(()=>{
+        alert('Cambios guardados')
+      })
+      .catch((error)=>{
+        console.log(error)
+      })
+    }
+
   }
   fileChangeEvent(event: any): void {
     this.imageChangedEvent = event;
