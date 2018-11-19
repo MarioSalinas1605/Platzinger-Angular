339c5a5f8ee883bce18d2ec4b496dffd86ed2026
MarioSalinas <mario_1605@live.com.mx>
2018-11-19 08:07:38 -0600

    Getting informaction from Firebase in section home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,12 +12,30 @@ import { Router } from '@angular/router';
 export class HomeComponent implements OnInit {
   friends: User[]
   query: string = ''
+  user: User
   constructor(private userService: UserService, private authenticationService: AuthenticationService, private router: Router) {
     this.userService.getUsers().valueChanges().subscribe(
       (data: User[])=>{
         this.friends = data
       },
       (error)=>{console.log(error)})
+
+      this.authenticationService.getStatus().subscribe(
+        (status)=>{
+          this.userService.getUserById(status.uid).valueChanges().subscribe(
+            (data: User)=>{
+              this.user = data
+              console.log(this.user)
+            },
+            (error)=>{
+              console.log(error)
+            }
+          )
+        },
+        (error)=>{
+          console.log(error)
+        }
+      )
    }
 
   ngOnInit() {
